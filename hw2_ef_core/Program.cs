bbc7c0f9e224a59d326848e77bce8cde5f7714cf
Danil <super.danil501@gmail.com>
2024-11-09 21:17:25 -0700

    Add project files.

---
+++ hw2_ef_core/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2_ef_core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NorthWindEntities())
+            {
+                foreach (var item in db.Categories)
+                {
+                    Console.WriteLine($"Id: {item.CategoryID}, Name: {item.CategoryName}\n Description: {item.Description}");
+                }
+
+            }
+            using (var db = new NorthWindEntities())
+            {
+                db.Categories.Add(new Categories { CategoryName = "Electronics", Description = "Phones, PC, etc." });
+                db.SaveChanges();
+            }
+            using (var db = new NorthWindEntities())
+            {
+                Console.WriteLine("--------------------------------------");
+                foreach (var item in db.Categories)
+                {
+                    Console.WriteLine($"Id: {item.CategoryID}, Name: {item.CategoryName}\n Description: {item.Description} {item.Picture}");
+                }
+            }
+            using (var db = new NorthWindEntities())
+            {
+                Console.WriteLine("--------------------------------------");
+                var query = db.Categories.Where(c => c.CategoryName == "Electronics");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.CategoryName);
+                }
+            }
+
+            using(var db = new NorthWindEntities())
+            {
+                var categorieToDelete = db.Categories.FirstOrDefault(c => c.CategoryName == "Electronics");
+                db.Categories.Remove(categorieToDelete);
+                db.SaveChanges();
+            }
+            using (var db = new NorthWindEntities())
+            {
+                Console.WriteLine("--------------------------------------");
+                foreach (var item in db.Categories)
+                {
+                    Console.WriteLine($"Id: {item.CategoryID}, Name: {item.CategoryName}\n Description: {item.Description} {item.Picture}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ hw2_ef_core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("hw2_ef_core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hw2_ef_core")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed3165d9-c190-4071-a1f1-dfc8b91fce04")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
